d85bd6061441728d493b4ee00993450e131f4d50
aymenrepo <aymensegnirepo@gmail.com>
2018-04-02 03:04:18 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="git_flask",
+    version="0.1",
+    url="https://github.com/aymenrepo/flask_python_api_rest_git_repo",
+    author="Aymen SEGNI",
+    author_email="",
+    description="Python REST API using Flask to manage Git repo",
+    long_description="",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3.7'
+    ],
+)
